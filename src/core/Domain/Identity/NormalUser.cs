a1c49b18ded7e4e790924df8497044b256f450da
Amir <sagan.5050@gmail.com>
2024-11-28 01:40:17 +0200

    add identity jwt

---
+++ src/core/Domain/Identity/NormalUser.cs
@@ -0,0 +1,5 @@
+namespace Domain.Identity;
+
+public class NormalUser : User
+{
+}
---
+++ src/core/Domain/Identity/User.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Identity;
+public abstract class User : IdentityUser
+{
+    [Required]
+    public string FirstName { get; set; } 
+    [Required]
+    public string LastName { get; set; } 
+    public string FullName => $"{FirstName} {LastName}";
+    public bool OAuthUser { get; set; }
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiryTime { get; set; }
+}
